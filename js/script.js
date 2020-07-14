b7ed140d2e872c0d587570ce87919f1a69ba296b
Riccardo Colombo <rikyregna@gmail.com>
2020-07-14 16:13:23 +0200

    // comment

--- js/script.js
+++ js/script.js
@@ -10,13 +10,18 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 // 0. inizializzo nuova var a stringa vuota
 var elemento = "";
 
+// ciclo for per ciclare la i
 for (var i = 1; i <= 100; i++) {
+  // se è multiplo di entrambi
   if (i % 3 == 0 && i % 5 == 0) {
     elemento += "<li>FizzBuzz</li>";
+    // se è multiplo di 5
   } else if (i % 5 == 0) {
     elemento += "<li>Buzz</li>";
+    // se è multiplo di 3
   } else if (i % 3 == 0) {
     elemento += "<li>Fizz</li>";
+    // altrimenti stampo i
   } else {
     elemento += "<li>" + i + "</li>";
   }
