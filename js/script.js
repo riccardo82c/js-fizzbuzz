cb1b914141099a50a27be1ca3546682462dfa7af
Riccardo Colombo <rikyregna@gmail.com>
2020-07-14 16:28:35 +0200

    little mod

--- js/script.js
+++ js/script.js
@@ -10,21 +10,23 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 // 0. inizializzo nuova var a stringa vuota
 var elemento = "";
 
-// ciclo for per ciclare la i
+// 1. ciclo for per ciclare la i
 for (var i = 1; i <= 100; i++) {
-  // se è multiplo di entrambi
-  if (i % 3 == 0 && i % 5 == 0) {
+  // 1.1 se è multiplo di entrambi (come prima condizione perchè va esclusa subito)
+  // oppure if (i % 3 == 0 && i % 5 == 0)
+  if (i % (3 * 5) == 0) {
     elemento += "<li>FizzBuzz</li>";
-    // se è multiplo di 5
+    // 1.2 se è SOLO multiplo di 5
   } else if (i % 5 == 0) {
     elemento += "<li>Buzz</li>";
-    // se è multiplo di 3
+    // 1.3 se è SOLO multiplo di 3
   } else if (i % 3 == 0) {
     elemento += "<li>Fizz</li>";
-    // altrimenti stampo i
+    // 1.4 ALTRIMENTI stampo i
   } else {
     elemento += "<li>" + i + "</li>";
   }
 }
 
+// 2. stampa a video dell'elemento in un <ul>
 document.getElementById("elemento").innerHTML = elemento;
